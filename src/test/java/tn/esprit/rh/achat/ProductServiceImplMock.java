a927dd6e83e5538b4178185aa804aa808d172d22
OumaimaChebbi-Esprit <chebbi.oumaima@esprit.tn>
2024-05-19 17:18:34 +0100

    Adding testRetrieveAll method in ProductServiceImplMock class

--- src/test/java/tn/esprit/rh/achat/ProductServiceImplMock.java
+++ src/test/java/tn/esprit/rh/achat/ProductServiceImplMock.java
@@ -1,4 +1,46 @@
 package tn.esprit.rh.achat;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.rh.achat.entities.Produit;
+import tn.esprit.rh.achat.repositories.ProduitRepository;
+import tn.esprit.rh.achat.services.ProduitServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ExtendWith(MockitoExtension.class)
 public class ProductServiceImplMock {
+
+    @Mock
+    ProduitRepository productRepository;
+
+    @InjectMocks
+    ProduitServiceImpl productService;
+
+    List<Produit> products = new ArrayList<Produit>() {
+        {
+            add(new Produit());
+            add(new Produit());
+            add(new Produit());
+        }
+    };
+
+    @Test
+    public void testRetrieveAll() {
+        Mockito.when(productRepository.findAll()).thenReturn(products);
+        List<Produit> retrievedProducts = productService.retrieveAllProduits();
+        Assertions.assertEquals(3, retrievedProducts.size(), "The expected size is : " + products.size());
+    }
+
 }
