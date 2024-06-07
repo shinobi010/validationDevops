127710e0f2046c7e126e3fd06f5fb65ef7c1e7bc
Doghri <hadhami.doghri@esprit.tn>
2024-06-08 00:04:12 +0100

    test class update crud

--- src/test/java/tn.esprit.rh.achat.service/DetailFournisseurServiceImplMockTest.java
+++ src/test/java/tn.esprit.rh.achat.service/DetailFournisseurServiceImplMockTest.java
@@ -1,21 +1,31 @@
 package tn.esprit.rh.achat.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import tn.esprit.rh.achat.entities.CategorieFournisseur;
 import tn.esprit.rh.achat.entities.DetailFournisseur;
 import tn.esprit.rh.achat.entities.Fournisseur;
 import tn.esprit.rh.achat.repositories.DetailFournisseurRepository;
 import tn.esprit.rh.achat.repositories.FournisseurRepository;
 import tn.esprit.rh.achat.services.FournisseurServiceImpl;
+
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class DetailFournisseurServiceImplMockTest {
 
     @Mock
@@ -31,27 +41,75 @@ class DetailFournisseurServiceImplMockTest {
     void setUp() {
         MockitoAnnotations.initMocks(this);
     }
+    DetailFournisseur detailFournisseur1 = new DetailFournisseur(1L,"four1@example.com",new Date(),"1 Rue de four1","MAT1",null);
+    Fournisseur fournisseur1 = new Fournisseur(1L,"001","Fournisseur1", CategorieFournisseur.CONVENTIONNE,null,null,detailFournisseur1);
 
+    DetailFournisseur detailFournisseur2 = new DetailFournisseur(2L,"four2@example.com",new Date(),"2 Rue de four2","MAT2",null);
+    Fournisseur fournisseur2 = new Fournisseur(1L,"002","Fournisseur2", CategorieFournisseur.CONVENTIONNE,null,null,detailFournisseur2);
+
+    DetailFournisseur detailFournisseur3 = new DetailFournisseur(3L,"four3@example.com",new Date(),"3 Rue de four3","MAT3",null);
+    Fournisseur fournisseur3 = new Fournisseur(1L,"003","Fournisseur3", CategorieFournisseur.CONVENTIONNE,null,null,detailFournisseur3);
+
+
+    List<Fournisseur> fournisseurs = new ArrayList<Fournisseur>() {
+        {
+            add(fournisseur1);
+            add(fournisseur2);
+            add(fournisseur3);
+        }
+    };
+    List<DetailFournisseur> detailsfournisseurs = new ArrayList<DetailFournisseur>() {
+        {
+            add(detailFournisseur1);
+            add(detailFournisseur2);
+            add(detailFournisseur3);
+        }
+    };
     @Test
-    void testAddDetailFournisseur() {
-        // Création d'un détail fournisseur simulé
-        DetailFournisseur detailFournisseur = new DetailFournisseur();
-        detailFournisseur.setEmail("test@example.com");
-        detailFournisseur.setDateDebutCollaboration(new Date());
-        detailFournisseur.setAdresse("123 Rue de Test");
-        detailFournisseur.setMatricule("MAT123");
-        Fournisseur fournisseur = new Fournisseur();
-        fournisseur.setDetailFournisseur(detailFournisseur);
-        fournisseur.setLibelle("Fournisseur1");
+    void testAddFournisseur() {
 
-        // Simuler le comportement du repository lorsqu'il sauvegarde le détail fournisseur
-        when(detailFournisseurRepository.save(any(DetailFournisseur.class))).thenReturn(detailFournisseur);
+        Mockito.when(fournisseurRepository.save(any(Fournisseur.class)))
+                .thenReturn(fournisseur1)
+                .thenReturn(fournisseur2)
+                .thenReturn(fournisseur3);
 
-        // Appeler la méthode pour ajouter un détail fournisseur
-        Fournisseur savedFournisseur = fournisseurService.addFournisseur(fournisseur);
+        Fournisseur savedFournisseur1 = fournisseurService.addFournisseur(fournisseur1);
+        Fournisseur savedFournisseur2 = fournisseurService.addFournisseur(fournisseur2);
+        Fournisseur savedFournisseur3 = fournisseurService.addFournisseur(fournisseur3);
 
-        // Vérifier si le détail fournisseur a été sauvegardé correctement
-        assertNotNull(savedFournisseur);
-        assertEquals(fournisseur.getDetailFournisseur(), savedFournisseur.getDetailFournisseur());
+        assertNotNull(savedFournisseur1);
+        assertEquals(fournisseur1.getDetailFournisseur(), savedFournisseur1.getDetailFournisseur());
+
+        assertNotNull(savedFournisseur2);
+        assertEquals(fournisseur2.getDetailFournisseur(), savedFournisseur2.getDetailFournisseur());
+
+        assertNotNull(savedFournisseur3);
+        assertEquals(fournisseur3.getDetailFournisseur(), savedFournisseur3.getDetailFournisseur());
+    }
+    @Test
+    void testRetrieveAllFournisseurs() {
+        Mockito.when(fournisseurRepository.findAll()).thenReturn(fournisseurs);
+        List<Fournisseur> retrievedProducts = fournisseurService.retrieveAllFournisseurs();
+        Assertions.assertEquals(3, retrievedProducts.size(), "The expected size is : " + fournisseurs.size());
+    }
+    @Test
+    void testRetrieveFournisseur () {
+        Mockito.when(fournisseurRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(fournisseur3));
+        Fournisseur product1 = fournisseurService.retrieveFournisseur(3L);
+        Assertions.assertNotNull(product1, "THE RETRIEVED FOURNISSEUR SHOULD NOT BE NULL§");
+    }
+    @Test
+    void testRemoveProduct() {
+        Long idFournissur = 1L;
+        Mockito.doNothing().when(fournisseurRepository).deleteById(Mockito.anyLong());
+        fournisseurService.deleteFournisseur(idFournissur);
+        Mockito.verify(fournisseurRepository, Mockito.times(1)).deleteById(idFournissur);
+    }
+    @Test
+    void testUpdateProduct() {
+        Mockito.when(fournisseurRepository.save(Mockito.any(Fournisseur.class))).thenReturn(fournisseur3);
+        Fournisseur updatedFournisseur = fournisseurService.updateFournisseur(fournisseur3);
+        Assertions.assertEquals(fournisseur3.getIdFournisseur(), updatedFournisseur.getIdFournisseur(), "The updated fournisseur must have the same id as the old one");
+        Assertions.assertNotNull(updatedFournisseur, "THE UPDATED FOURNISSEUR SHOULD NOT BE NULL!");
     }
 }
